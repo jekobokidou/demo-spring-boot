14fcc6a55071a6476b0719e0ac657839c3f79fcf
Stéphane Ekobo <mymac@macbook-pro-de-mymac.home>
2021-05-14 01:36:01 +0200

    First commit

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins="http://localhost:3000")
+public class HelloWorldController {
+
+	@RequestMapping(method = RequestMethod.GET,path="/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path="/hello-world-get")
+	public String helloWorldGet() {
+		return "Hello World Get";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World Bean");
+	}
+	
+	@GetMapping(path="/hello-world-bean/var/{name}")
+	public HelloWorldBean helloWorldBeanPath(@PathVariable String name ){
+		return new HelloWorldBean(String.format("Hello World Bean path %s", name));
+	}
+}
